6ad82d991f3830a6b5b8e83b98b2336b20545187
Jermin Tiu <jermin2@gmail.com>
2019-09-05 22:51:57 +1200

    removed paid/reg from the Event_person view

--- application/views/event_person/index.php
+++ application/views/event_person/index.php
@@ -7,17 +7,15 @@
 		<th>Full Name</th>
 		<th>Event Name</th>
 		<th>Attendence</th>
-		<th>Comment</th>
 		<th>Actions</th>
     </tr>
 	<?php foreach($event_people as $c){ ?>
     <tr>
 		<td><?php echo $c['full_name']; ?></td>
 		<td><?php echo $c['event_name']; ?></td>
-		<td><a href="#" class="btn btn-xs <?php echo ($c['registered']==1 ? 'btn-success' : 'btn-secondary' )?>">Reg</a> 
+		<td><!-- <a href="#" class="btn btn-xs <?php echo ($c['registered']==1 ? 'btn-success' : 'btn-secondary' )?>">Reg</a> -->
 				<a href="#" class="btn btn-xs <?php echo ($c['attended']==1 ? 'btn-success' : 'btn-secondary' )?>">Atten</a>		
-				<a href="#" class="btn btn-xs <?php echo ($c['paid']==1 ? 'btn-success' : 'btn-secondary' )?>">Paid</a>		</td>	
-		<td><?php echo $c['comment']; ?></td>
+				<!-- <a href="#" class="btn btn-xs <?php echo ($c['paid']==1 ? 'btn-success' : 'btn-secondary' )?>">Paid</a>	-->	</td>	
 		<td>
             <a href="<?php echo site_url('event_person/edit/'.$c['event_people_id']); ?>" class="btn btn-info btn-xs">Edit</a> 
             <a href="<?php echo site_url('event_person/remove/'.$c['event_people_id']); ?>" class="btn btn-danger btn-xs">Delete</a>
