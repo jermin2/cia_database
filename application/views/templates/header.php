37b6147f25a444c63c3aacc47df6f2dc7136a7b7
Jermin Tiu <jermin2@gmail.com>
2019-09-06 13:32:51 +1200

    trying to sort out mobile view

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset = "utf-8">
-<title>Church in Auckland Database</title> 	
+	<title>Church in Auckland Database</title> 	
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel = "stylesheet" type = "text/css" 
 		href = "<?php echo asset_url(); ?>css/bootstrap.min.css?version=1"> 
 	<script type = 'text/javascript' src = "<?php echo asset_url(); 
